be9dcc9a62b9035d7da9f67e1502e8a5e391430d
Oschenker <oschenker@gmail.com>
2022-10-30 22:48:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '5607906910:AAEZllfu1l3xpaF-DJAih5jKmrx-xFzShwQ';
+const token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
